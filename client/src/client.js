5d051f1636fefdb96e9245bc0002edd33840bff2
Ferid HELALI <helaliferid@gmail.com>
2022-05-25 13:24:25 +0100

    fix cors issue

--- client/src/client.js
+++ client/src/client.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io('http://localhost:3000');
 
 socket.on("connect", () => {
     console.log(socket.id); 
--- server/server.js
+++ server/server.js
@@ -1,25 +1,30 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors')
+const http = require('http');
+
+
 const app = express();
+app.options('*', cors())
+app.use(cors())
 
 
-const server = require('http').createServer(app);
-
-const io = require('socket.io')(server,{
-    cors: {
-      origin: "http://localhost:3000"
-    }
-  });
-
-io.on('connection', client => {
-  client.on('event', data => { 
-      console.log(` event recievd from client with payload:${JSON.stringify(data)}`)
-  });
-  client.on('disconnect', () => {
-    console.log(`${JSON.stringify(client)} is disconnected`)
-
-  });
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {cors: {
+    origin: "http://localhost:1234",
+  }
 });
 
-server.listen(3000,()=>{
-    console.log('Server is running on http://localhost:3000')
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Websocke :)' })
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
 });
